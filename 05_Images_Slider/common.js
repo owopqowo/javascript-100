4df63bc474edc1c3bf04c007e4d547871d20b22b
owopqowo <54467533+owopqowo@users.noreply.github.com>
2020-10-22 09:17:56 +0900

    images slider

---
+++ 05_Images_Slider/common.js
@@ -0,0 +1,28 @@
+const slide = document.querySelector('.slide');
+const btn = document.querySelectorAll('.slider-navi button');
+const img = [
+    'img1.jpg',
+    'img2.jpg',
+    'img3.jpg',
+    'img4.jpg',
+]
+let slideCount = 0;
+
+btn.forEach(function(element){
+    element.addEventListener('click', function(){
+        if(element.classList.contains('btn-prev')){
+            if(slideCount <= 0) {
+                slideCount = img.length - 1;
+            } else {
+                --slideCount;
+            }
+        } else {
+            if(slideCount >= img.length - 1) {
+                slideCount = 0;
+            } else {
+                ++slideCount;
+            }
+        }
+        slide.style = `background-image: url(img/${img[slideCount]})`
+    });
+});
