cf38c56360416173f2e5e1b8c1909ea0d808f034
owopqowo <54467533+owopqowo@users.noreply.github.com>
2020-10-23 15:48:50 +0900

    calculator

---
+++ 08_Calculator/common.js
@@ -0,0 +1,24 @@
+const input = document.querySelector('input');
+const btnList = document.querySelectorAll('.btn-list button');
+const btnEqual = document.querySelector('.btn-equal');
+
+
+btnList.forEach(function(element){
+    element.addEventListener('click', function(){
+        const inputVal = input.value;
+        const dataText = element.textContent;
+        if(dataText == 'C') {
+            input.value = '';
+        } else {
+            input.value = inputVal+dataText;
+        }
+    });
+});
+
+btnEqual.addEventListener('click', function(){
+    let inputVal = input.value;
+    inputVal = inputVal.replace("×", "*");
+    inputVal = inputVal.replace("÷", "/");
+    const anwser = eval(inputVal)
+    input.value = anwser;
+});
